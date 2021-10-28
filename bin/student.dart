8afc72c5009b06de802b2961267888389ab1b427
brangkak3 <fauzanwadi7@gmail.com>
2021-10-28 09:20:49 +0800

    version 4 jika tidak ada isi gamti jadi ;

--- bin/student.dart
+++ bin/student.dart
@@ -1,9 +1,7 @@
 import 'person.dart';
 
 class Student extends Person {
-  Student({String studentName = 'Studen baru'}) : super(name: studentName) {
-    print('constuctor student');
-  }
+  Student({String studentName = 'Studen baru'}) : super(name: studentName);
 }
 
 // : super('student baru')  //jika ada parameter
